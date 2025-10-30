e0ebba06a19d8d5664902ac50dfe1328b7038bba
subterraneanbob <4823336+subterraneanbob@users.noreply.github.com>
2025-10-30 21:20:55 +0300

    Add docstring for user exceptions

--- valutatrade_hub/core/exceptions.py
+++ valutatrade_hub/core/exceptions.py
@@ -1,13 +1,29 @@
 class UserError(Exception):
+    """
+    Базовый класс для ошибкок, которые происходят из-за неверного ввода
+    пользователя.
+    """
+
     pass
 
 
 class ExchangeRateUnavailableError(UserError):
+    """
+    Ошибка возникает, когда курс обмена валюты не найден в локальном кеше.
+    Выбрасывается в методе `ExchangeRates.get_exchange_rate`.
+    """
+
     def __init__(self, from_currency: str):
-        super().__init__(f"Курс для '{from_currency}' не найден в кеше.")
+        super().__init__(f"Курс обмена для '{from_currency}' не найден в кеше.")
 
 
 class InsufficientFundsError(UserError):
+    """
+    Ошибка возникает, если в кошельке недостаточно средств для проведения
+    транзакции. Выбрасывается в методе `Wallet.withdraw` и в функции
+    `sell` модуля `core.usecases`.
+    """
+
     def __init__(self, currency: str, available: float, required: float):
         message = (
             "Недостаточно средств: "
@@ -18,6 +34,12 @@ class InsufficientFundsError(UserError):
 
 
 class WalletNotFound(UserError):
+    """
+    Ошибка происходит, если попытаться продать валюту, если кошелёк для неё
+    ещё не создан. Выбрасывается в методе `Portfolio.get_wallet` и в функции
+    `sell` модуля `core.usecases`.
+    """
+
     def __init__(self, currency: str):
         message = (
             f"У вас нет кошелька '{currency}'. Добавьте валюту: "
@@ -27,10 +49,21 @@ class WalletNotFound(UserError):
 
 
 class CurrencyNotFoundError(UserError):
+    """
+    Ошибка происходит, если пользователь указал валюту, неизвестную системе.
+    Выбрасывается в функции `get_currency` модуля `core.currencies`.
+    """
+
     def __init__(self, currency: str):
         super().__init__(f"Валюта '{currency}' не найдена.")
 
 
 class InvalidAmountError(UserError):
+    """
+    Ошибка возникает, если пользователь указал неположительное значение валюты.
+    Выбрасывается в методах `Wallet.deposit` и `Wallet.withdraw`, а также
+    в функциях `buy`, `sell` модуля `core.usecases`.
+    """
+
     def __init__(self, attr_name: str):
         super().__init__(f"'{attr_name}' должен быть положительным числом.")
