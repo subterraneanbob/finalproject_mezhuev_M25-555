d88117d94afc0120ce192f427d328bd720a3118c
subterraneanbob <4823336+subterraneanbob@users.noreply.github.com>
2025-11-01 22:02:36 +0300

    Move SingletonMeta class

--- valutatrade_hub/infra/settings.py
+++ valutatrade_hub/infra/settings.py
@@ -1,11 +1,29 @@
 import tomllib
 from typing import Any
 
-from .singleton_meta import SingletonMeta
-
 PYPROJECT_FILE_PATH = "pyproject.toml"
 TOOL = "tool"
 SECTION = "valutatrade"
+
+
+class SingletonMeta(type):
+    """
+    Метакласс для создания классов, которые могут иметь только один экземпляр.
+    Реализация через метакласс удобна из-за простоты: класс описывается
+    как обычно, нужно только указать metaclass в определении.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Создаёт только один экземпляр класса, который использует этот метакласс
+        в определении.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
 
 
 class SettingsLoader(metaclass=SingletonMeta):
